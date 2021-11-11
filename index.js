cc13089076ab6e5ed7b4dcc50254d0f34f534855
Noah A Klem <noahaklem@gmail.com>
2021-11-10 17:47:30 -0700

    commit tree 83b6b8d3cc93a5a3c2febbc2b47a21bea026434a

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Your code here
+function mapToNegativize(array) {
+    let results = [];
+
+    for (let i = 0; i < array.length; i++) {
+        results.push(array[i] * -1)
+    }
+    return results
+}
+
+function mapToNoChange(array) {
+    let results = [];
+
+    for (let i = 0; i < array.length; i++) {
+        results.push(array[i])
+    }
+    return results
+}
+
+function mapToDouble(array) {
+    let results = [];
+
+    for (let i = 0; i < array.length; i++) {
+        results.push(array[i] * 2)
+    }
+    return results
+}
+
+function mapToSquare(array) {
+    let results = [];
+
+    for (let i = 0; i < array.length; i++) {
+        results.push(array[i] ** 2)
+    }
+    return results
+}
+
+function reduceToTotal(array, start = 0) {
+    let accum = start;
+
+    for (let i = 0; i < array.length; i++) {
+        accum = array[i] + accum
+    }
+    return accum
+}
+
+function reduceToAllTrue(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (!array[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i]) return true
+    }
+    return false
+}
